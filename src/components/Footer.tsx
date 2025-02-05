6f5d51bbb7e23061c6831447cd344b8298642208
kaleabkindu <kkmichaelstarkk@gmail.com>
2025-02-06 01:36:32 +0300

    minor change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,6 @@ type Props = {}
 const Footer = (props: Props) => {
   return (
     <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-    <div className='py-10 text-2xl font-bold'>API: {process.env.API}</div>
     <a
       href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -73,6 +72,7 @@ const Footer = (props: Props) => {
         Instantly deploy your Next.js site to a shareable URL with Vercel.
       </p>
     </a>
+    <div className='py-10 text-2xl font-bold'>API: {process.env.API}</div>
   </div>
   )
 }
